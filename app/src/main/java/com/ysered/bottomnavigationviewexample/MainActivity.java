04173a5fad3f5960a0b1202f6a9f9e3bac1e8039
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-02-09 01:46:17 +0200

    Create methods to load fragment.

--- app/src/main/java/com/ysered/bottomnavigationviewexample/MainActivity.java
+++ app/src/main/java/com/ysered/bottomnavigationviewexample/MainActivity.java
@@ -3,16 +3,33 @@ package com.ysered.bottomnavigationviewexample;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.util.SparseArrayCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private static final int TAB_COUNT = 3;
+
+    //final SparseArrayCompat<Fragment> fragments = new SparseArrayCompat<>(TAB_COUNT);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Fragment fragment = new ProfileFragment();
+        loadFragment(fragment);
+
+        /*fragments.put(R.id.itemProfile, fragment);
+        fragments.put(R.id.itemMap, new MapFragment());
+        // FIXME: image views shows when fragment created once
+        fragments.put(R.id.itemDashboard, new DashboardFragment());*/
+
         final BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -30,14 +47,25 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 if (fragment != null) {
-                    getSupportFragmentManager().beginTransaction()
-                            .setCustomAnimations(R.anim.slide_in_top, R.anim.slide_out_bottom,
-                                    R.anim.slide_in_top, R.anim.slide_out_bottom)
-                            .replace(R.id.containerFrame, fragment)
-                            .commit();
+                    loadFragment(fragment);
                 }
                 return true;
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        loadFragment(fragment, false);
+    }
+
+    private void loadFragment(Fragment fragment, boolean addToBackStack) {
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.slide_in_top, R.anim.slide_out_bottom,
+                R.anim.slide_in_top, R.anim.slide_out_bottom)
+                .replace(R.id.containerFrame, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
     }
 }
